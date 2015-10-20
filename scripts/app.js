17ed6e9719ed3890f6ec969361d349d527bd348b
mike01101011 <mike01101011@gmail.com>
2015-10-20 11:07:12 -0400

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+jQuery = {};
+
+$(function(){
+//console.log('01 $Ready');
+
+	jQuery.initializeApiApp();
+});
+
+// 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02 02
+
+	jQuery.initialize = function () {
+//console.log('02 initialize');
+
+			// jQuery.functionName();
+	};
